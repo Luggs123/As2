06445040531174594668153c02c5d787818630a7
shiftgenesis <pgaudnik@gmail.com>
2016-10-06 22:02:19 -0400

    WIP clsUnit

---
+++ src/packUnit/clsUnit.java
@@ -0,0 +1,22 @@
+package packUnit;
+
+
+public class clsUnit {
+    protected float unitPrice;
+    protected int qtyOnHand;
+    protected String itemDsc;
+    protected String warehouseCity;
+   
+    
+    public double computeTotal(float unitPrice, int qtyOnHand) {
+        this.unitPrice = unitPrice;
+        this.qtyOnHand = qtyOnHand;
+        double cadTotal = (unitPrice * qtyOnHand);
+        
+        /* Computes the total cost of a Unit by multiplying the quantity on hand 
+           in warehouse by the unit price(in CAD). 
+        */ 
+        return cadTotal;
+    }
+    
+}
