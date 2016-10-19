f65e0cbc02ca3e61ae71f0227cfb1a31f10b256c
Luggs123 <sunsplashin@hotmail.com>
2016-10-18 22:40:51 -0400

    Whoops I am not good with computers.

--- src/packMain/clsMain.java
+++ src/packMain/clsMain.java
@@ -413,20 +413,4 @@ public class clsMain extends JFrame implements interfaceMain
 			eurField.setEditable(false);
 		}
 	}
-
-	public static <K, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) 
-	{
-		Comparator<K> valueComparator =  new Comparator<K>() 
-		{
-			public int compare(K k1, K k2) 
-			{
-				int compare = map.get(k2).compareTo(map.get(k1));
-				if (compare == 0) return 1;
-				else return compare;
-			}
-		};
-		Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
-		sortedByValues.putAll(map);
-		return sortedByValues;
-	}
 }
